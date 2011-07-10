99a5595a1a8bc956c12cde0f7db80f42c9a01281
Devin Price <devin@wptheming.com>
2011-07-10 18:31:49 -0500

    Remove Twenty Eleven options panel.

--- options-check/functions.php
+++ options-check/functions.php
@@ -74,3 +74,13 @@ function of_sanitize_text_field($input) {
 }
 
 */
+
+/* 
+ * Turns off the default options panel from Twenty Eleven
+ */
+ 
+add_action('after_setup_theme','remove_twentyeleven_options', 100);
+
+function remove_twentyeleven_options() {
+	remove_action( 'admin_menu', 'twentyeleven_theme_options_add_page' );
+}
