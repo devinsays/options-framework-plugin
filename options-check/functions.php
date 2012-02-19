1a36c981d26621bc1ef6f4b6fd807d4478e4f69d
cravelight <cravelight@gmail.com>
2012-02-19 12:59:25 -0600

    Updated the of_get_option function to cache the theme options on the first request.  No need to hit the database multiple times per page request to get the same info.

--- options-check/functions.php
+++ options-check/functions.php
@@ -10,17 +10,22 @@
 if ( !function_exists( 'of_get_option' ) ) {
 function of_get_option($name, $default = false) {
 	
-	$optionsframework_settings = get_option('optionsframework');
+	static $options_array = NULL;
 	
-	// Gets the unique option id
-	$option_name = $optionsframework_settings['id'];
-	
-	if ( get_option($option_name) ) {
-		$options = get_option($option_name);
+	if ( !$options_array ) {
+		
+		$optionsframework_settings = get_option('optionsframework');
+
+		// Gets the unique option id
+		$option_name = $optionsframework_settings['id'];
+
+		if ( get_option($option_name) ) {
+			$options_array = get_option($option_name);
+		}
 	}
 		
-	if ( isset($options[$name]) ) {
-		return $options[$name];
+	if ( isset($options_array[$name]) ) {
+		return $options_array[$name];
 	} else {
 		return $default;
 	}
